aac6a08daef5eb86cc4242d2b8063d837f392f4f
timeend123 <timeend123@users.noreply.github.com>
2018-04-01 21:10:54 +0800

    Delete global.js

--- finance-client/src/app/global.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/* 定义一个全局的URL */
-exports.baseUrl = 'http://localhost:8011/controller';
-// export const baseUrl = 'http://localhost:8081/sclms';
